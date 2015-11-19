9a842c2f832759b53bf2993ba24a6d2a6d272670
pengshizhong <523724329@qq.com>
2015-11-19 22:52:19 +0800

    增加个pdo的query实例

---
+++ php/pdo_query.php
@@ -0,0 +1,26 @@
+<?php
+ 
+              #数据库连接
+              $dbtype = 'mysql';
+              $host = 'localhost';
+              $db = 'yii2basic';
+              $user = 'root';
+             $psw = '123456';
+             
+             $dsn = $dbtype . ':host=' . $host . ';' . 'dbname=' . $db;
+              try {
+                 $dbh = new PDO($dsn, $user, $psw, array(PDO::ATTR_PERSISTENT=>true));
+                 echo '连接成功<br>';
+             } catch(Exception $e) {
+                 die('Connect Failed Message: ' . $e->getMessage());
+             }
+             
+             #使用query函数查询
+             $sql = 'select * from user';
+             //$sql = 'lock tables user write';
+             //var_dump($dbh);
+             $query = $dbh->query($sql);
+              //var_dump($query);
+              $query->setFetchMode(PDO::FETCH_ASSOC);    //设置结果集返回格式,此处为关联数组,即不包含index下标
+              $rs = $query->fetchAll();
+              var_dump($rs);
